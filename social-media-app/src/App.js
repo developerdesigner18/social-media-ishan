6ebc57ebd43815e4b3602bc80650921e5986ce1c
developerdesigner18 <developer.designer18@gmail.com>
2022-06-06 11:58:10 +0530

    signup

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './component/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ishan <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am clever
-        </a>
-      </header>
+      <Signup/>
     </div>
   );
 }
